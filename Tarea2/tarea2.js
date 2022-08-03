3124c526478578886907b5e500fb1a6b42d220f8
Maximiliano Alonso <maximilianoalonso2012.ma@gmail.com>
2022-08-02 23:02:57 -0500

    Se agregan tareas

---
+++ Tarea2/tarea2.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Maximiliano",
+  32,
+  true,
+  new Date(1989, 08, 1989),
+  {
+    titulo: "La Belleza interna",
+    autor: "Eleonora Casano",
+    fecha: new Date(1989, 2, 1),
+    url: "https://www.amazon.es/dp/51515/",
+  },
+];
