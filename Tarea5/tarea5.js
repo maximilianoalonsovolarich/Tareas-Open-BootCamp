21818318350176a2311631d339b8d7248abfae0b
Maximiliano Alonso <maximilianoalonso2012.ma@gmail.com>
2022-08-02 23:42:24 -0500

    Se entrega tarea 5

---
+++ Tarea5/tarea5.js
@@ -0,0 +1,8 @@
+const altura_cm = 172;
+const altura_m = 1.72;
+const peso_kg = 82;
+
+const altura_red = Math.ceil(altura_m);
+const peso_red = Math.floor(peso_kg);
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
